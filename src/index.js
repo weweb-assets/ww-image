e5c332f8448917ac4599a229ef8d4bc2be27bb4e
Kramerbaf <nds.kram@gmail.com>
2018-06-13 09:43:29 +0200

    progress

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import wwObject from './wwObject.vue'
 
-const name = "ww-text";
+const name = "ww-image";
+const wwEnableMetrics = true;
 
 const addComponent = function () {
     if (window.vm) {
         window.vm.addComponent(name, wwObject);
+
+        wwLib.wwObject.registerContentType(
+            name,
+            {
+                type: name,
+                data: {
+                    url: '/img/no_image_selected.png',
+                    alt: {
+                        fr_FR: "webeo",
+                        en_GB: "webeo",
+                    },
+                    position: { x: 0, y: 0 },
+                    zoom: -1,
+                    ratio: -1,
+                    imgSize: {
+                        w: 1920,
+                        h: 1080
+                    },
+                    borderColor: '',
+                    classes: [],
+                    filterGradient: null,
+                    hover: {
+                        name: '',
+                        options: {}
+                    }
+                }
+            },
+            wwEnableMetrics
+        );
 
         return true;
     }
